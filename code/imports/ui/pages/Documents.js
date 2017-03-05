69600de0f34931efce5e0a848ad9ee137d1211a9
ninjaPixel <matthew@ninjaPixel.eu>
2017-03-05 12:46:52 +0000

    Initial commit of tutorial code

--- code/imports/ui/pages/Documents.js
+++ code/imports/ui/pages/Documents.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router';
 import { Row, Col, Button } from 'react-bootstrap';
 import DocumentsList from '../containers/DocumentsList.js';
 
@@ -9,12 +8,11 @@ const Documents = () => (
       <Col xs={ 12 }>
         <div className="page-header clearfix">
           <h4 className="pull-left">Documents</h4>
-          <Link to="/documents/new">
-            <Button
-              bsStyle="success"
-              className="pull-right"
-            >New Document</Button>
-          </Link>
+          <Button
+            bsStyle="success"
+            className="pull-right"
+            href="/documents/new"
+          >New Document</Button>
         </div>
         <DocumentsList />
       </Col>
